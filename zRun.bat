3548a46376be502961aa3ede010d5d27e699d3f4
Luke Zhang <lukezhang@microsoft.com>
2026-06-10 14:36:52 -0700

    Add zRun.bat script for vcpkg bootstrap and MSBuild integration

---
+++ zRun.bat
@@ -0,0 +1,12 @@
+
+
+REM  https://github.com/microsoft/vcpkg.git
+
+.\bootstrap-vcpkg.bat
+
+REM  Integrate with Visual Studio MSBuild. The next step is to enable user-wide vcpkg integration, this makes MSBuild aware of vcpkg's installation path.
+
+REM  .\vcpkg.exe integrate install
+
+REM  .\vcpkg.exe integrate remove
+
